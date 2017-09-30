79a2d1483a464f546e51981239f1b44077f3c780
pepemiyashiro <pepe.miyashiro@beauthentic.digital>
2017-09-30 10:26:38 -0500

    [update] Renaming things and minor css updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# sassy-vue2
+# vue2-typescript
 
 > A Vue.js project
 
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -18,9 +18,3 @@
     }
   }
 </script>
-
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-  @import '../assets/scss/global/global';
-</style>
